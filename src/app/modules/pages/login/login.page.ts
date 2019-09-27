04ae04312369b701acbc06b1564c93f5c629055a
santiago.yacelga <esyacelga@hotmail.com>
2019-09-27 15:39:06 -0500

    001

--- src/app/modules/pages/login/login.page.ts
+++ src/app/modules/pages/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {RegistroTipoUsuarioPersona} from '../../classes/persona/RegistroTipoUsuarioPersona';
+import {TipoUsuario} from '../../classes/persona/TipoUsuario';
+import {Sector} from '../../classes/persona/Sector';
+import {RegistroMensajes} from '../../classes/login/RegistroMensajes';
+import {FormGroup} from '@angular/forms';
+import {Usuario} from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+    objetoLogin = new RegistroTipoUsuarioPersona();
+    objTipoUsuario = new TipoUsuario();
+    lstSectores: Array<Sector>;
+    registoMensajes: RegistroMensajes = new RegistroMensajes();
+    error_messages = this.registoMensajes.error_messages;
+    loginForm: FormGroup;
+    ingresoForm;
 
-  constructor() { }
+    registerUser: Usuario = {
+        email: '',
+        password: '',
+        nombre: '',
+        avatar: ''
+    };
+
+
+    constructor() { }
 
   ngOnInit() {
   }
+
 
 }
