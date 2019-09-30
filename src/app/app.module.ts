0179a237e4e6546698bba4377d56d1d0273ee7a6
santiago.yacelga <esyacelga@hotmail.com>
2019-09-30 08:48:00 -0500

    Black Cat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {RestConectionModule} from './modules/system/generic/rest-conection/rest-conection.module';
 import {PipesModule} from './modules/pipes/pipes.module';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+import {Camera} from '@ionic-native/camera/ngx';
+import {FileTransfer} from '@ionic-native/file-transfer/ngx';
+import {OneSignal} from '@ionic-native/onesignal/ngx';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
     imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, RestConectionModule],
     providers: [
+        Camera,
+        FileTransfer,
         StatusBar,
+        Geolocation,
         PipesModule,
         SplashScreen,
+        OneSignal,
         {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
     ],
     bootstrap: [AppComponent]
