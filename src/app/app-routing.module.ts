bec512e8068520df8ab63933f4447119e8709897
santiago.yacelga <esyacelga@hotmail.com>
2019-10-09 14:54:49 -0500

    Página en blanco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ const routes: Routes = [
         path: 'main',
         loadChildren: () => import('./modules/pages/tabModule/tabs/tabs.module').then(m => m.TabsPageModule)
     },
-    {path: '', pathMatch: 'full', redirectTo: 'login'},
+    {path: '', pathMatch: 'full', redirectTo: 'black'},
     {path: 'login', loadChildren: './modules/pages/login/login.module#LoginPageModule'},
     {path: 'articulo', loadChildren: './modules/pages/mensajeria/articulo/articulo.module#ArticuloPageModule'},
     {path: 'segmento', loadChildren: './modules/pages/mensajeria/segmento/segmento.module#SegmentoPageModule'},
     {path: 'managment', loadChildren: './modules/pages/notificacion/managment/managment.module#ManagmentPageModule'},
     {path: 'sector', loadChildren: './modules/pages/persona/sector/sector.module#SectorPageModule'},
     {path: 'tipo-usuario', loadChildren: './modules/pages/persona/tipo-usuario/tipo-usuario.module#TipoUsuarioPageModule'},
-    {path: 'tipo-articulo', loadChildren: './modules/pages/mensajeria/tipo-articulo/tipo-articulo.module#TipoArticuloPageModule'}
+    {path: 'tipo-articulo', loadChildren: './modules/pages/mensajeria/tipo-articulo/tipo-articulo.module#TipoArticuloPageModule'},
+    {path: 'black', loadChildren: './modules/pages/login/black/black.module#BlackPageModule'}
 
 
 ];
